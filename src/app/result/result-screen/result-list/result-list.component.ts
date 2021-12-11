1f5758bd8315513a6e2d6ca6aeea69a157c30ea1
Kraylor <gameking.mohm@hotmail.com>
2021-12-11 23:06:45 +0100

    Made one example more detailed and set a comment in the ngOnInit method of result-list.component.ts to be made later when calls to backend are functional -Rob- TODO: should give list-items a max height

--- src/app/result/result-screen/result-list/result-list.component.ts
+++ src/app/result/result-screen/result-list/result-list.component.ts
@@ -8,16 +8,21 @@ import {ResultModel} from "./result-list-item/result-model/result.model";
 })
 export class ResultListComponent implements OnInit {
   results: ResultModel[] = [
-    new ResultModel("testname0", "testdescription0", "https://www.svdj.nl/"),
+    new ResultModel("TalentOntwikkeling", "De subsidieregeling talentontwikkeling is bedoeld om binnen de mediasector onderzoeksjournalistiek een impuls te geven via training, begeleiding, werkervaring of specialisatie. Om de kwaliteit van de onderzoeksjournalistiek in Nederland op niveau te houden en meer journalisten de kans te bieden zich te bekwamen in dit specifieke deel van het vak, richten we ons op mediaorganisaties die hun expertise op dit vlak verder willen delen.", "https://www.svdj.nl/regeling/talentontwikkeling/"),
     new ResultModel("testname1", "testdescription1", "https://www.svdj.nl/"),
     new ResultModel("testname2", "testdescription2", "https://youtu.be/dQw4w9WgXcQ"),
     new ResultModel("testname3", "testdescription3", "https://www.svdj.nl/"),
     new ResultModel("testname4", "testdescription4", "https://www.svdj.nl/")
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    /*
+    for each result:
+    addResult(result)
+     */
   }
 
   addResult(resultModel: ResultModel) {
