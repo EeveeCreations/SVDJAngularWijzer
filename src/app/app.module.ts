2c78edeca0f7e42e014d6f5ebbec323c8c8c710f
Kraylor <gameking.mohm@hotmail.com>
2021-12-13 01:07:04 +0100

    vraag scherm toegevoegd. switch inhoud op basis van questionlist in question-screen.component.ts waar nu dummy info instaat. CSS nog niet opgemaakt. -Rob- TODO: onNext logt error wanneer doorgeklikt als array op is. onprevious doet dat voor welke reden dan ook niet. breekt de webapp niet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuestionScreenComponent } from './subsidiewijzer/question-screen/question-screen.component';
+import { QuestionComponent } from './subsidiewijzer/question-screen/question/question.component';
+import { AnswerComponent } from './subsidiewijzer/question-screen/answer/answer.component';
+import { ExtraInfoComponent } from './subsidiewijzer/question-screen/answer/extra-info/extra-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionScreenComponent,
+    QuestionComponent,
+    AnswerComponent,
+    ExtraInfoComponent
   ],
   imports: [
     BrowserModule
