0eb9e16b2cdb40ca09a265bae53d268758a0fc6d
Kraylor <gameking.mohm@hotmail.com>
2021-12-17 11:35:40 +0100

    begin scharm aan vragenlijst gekoppeld -Rob-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { QuestionScreenComponent } from './subsidiewijzer/question-screen/question-screen.component';
 import { QuestionComponent } from './subsidiewijzer/question-screen/question/question.component';
 import { AnswerComponent } from './subsidiewijzer/question-screen/answer/answer.component';
 import { ExtraInfoComponent } from './subsidiewijzer/question-screen/answer/extra-info/extra-info.component';
@@ -13,7 +12,6 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
   declarations: [
     AppComponent,
     routingComponents,
-    QuestionScreenComponent,
     QuestionComponent,
     AnswerComponent,
     ExtraInfoComponent
