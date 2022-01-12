891eb0cb35a51649b148295a42d722fc7a7d39ec
MisterQuack <brandonplokker@live.nl>
2022-01-12 02:52:48 +0100

    More-info component toegevoegd en question eraan

---
+++ src/app/subsidiewijzer/question-screen/more-info/more-info.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Question } from 'src/app/shared/models/question.model';
+
+@Component({
+  selector: 'app-more-info',
+  templateUrl: './more-info.component.html',
+  styleUrls: ['./more-info.component.css']
+})
+export class MoreInfoComponent implements OnInit {
+  @Input() question: Question;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
