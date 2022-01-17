b410a5f5e6051897fdf25e0d5a9305a15c0d8ceb
MisterQuack <brandonplokker@live.nl>
2022-01-17 17:40:02 +0100

    Forget wachtwoord sub components css

--- src/app/admin/forgot-password/send-token/send-token.component.ts
+++ src/app/admin/forgot-password/send-token/send-token.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-send-token',
   templateUrl: './send-token.component.html',
-  styleUrls: ['./send-token.component.css']
+  styleUrls: ['../forgot-password.component.css']
 })
 export class SendTokenComponent implements OnInit {
 
--- src/app/admin/forgot-password/verify-token/verify-token.component.ts
+++ src/app/admin/forgot-password/verify-token/verify-token.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-verify-token',
   templateUrl: './verify-token.component.html',
-  styleUrls: ['./verify-token.component.css']
+  styleUrls: ['./../forgot-password.component.css']
 })
 export class VerifyTokenComponent implements OnInit {
 
