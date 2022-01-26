16804ce77bc563f8cd78ee9cbbb61102287696e1
MisterQuack <brandonplokker@live.nl>
2022-01-26 06:16:41 +0100

    slider voor nextQuestion of advice werkt

--- src/app/admin/edit-question/edit-question.component.ts
+++ src/app/admin/edit-question/edit-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Advice } from 'src/app/shared/models/advice.model';
 import { Answer } from 'src/app/shared/models/answer.model';
 import { Question } from 'src/app/shared/models/question.model';
 import { StartRequestService } from 'src/app/shared/request/start-request.service';
@@ -10,6 +11,7 @@ import { StartRequestService } from 'src/app/shared/request/start-request.servic
 })
 export class EditQuestionComponent implements OnInit {
   questions: Question[] = [];
+  advices: Advice[] = [];
 
   currentQuestion: Question;
   errorLabel: String;
@@ -19,7 +21,11 @@ export class EditQuestionComponent implements OnInit {
   ngOnInit(): void {
     this.startRequestService.makeRequestOfQuestion("get", "all", null).subscribe(response => {
       this.questions = response;
-    })
+    });
+
+    this.startRequestService.makeRequestOfAdvice("get", "all", null).subscribe(response => {
+      this.advices = response;
+    });
   }
 
   newQuestion() {
