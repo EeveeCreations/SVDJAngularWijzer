8198b20b15f29a84b1df589584bd3e0acb06b6a8
MisterQuack <brandonplokker@live.nl>
2022-01-27 06:58:14 +0100

    Advies werkt ook nu

--- src/app/subsidiewijzer/advice/advice.component.ts
+++ src/app/subsidiewijzer/advice/advice.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Advice } from 'src/app/shared/models/advice.model';
+import { StartRequestService } from 'src/app/shared/request/start-request.service';
 
 @Component({
   selector: 'app-advice',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advice.component.css']
 })
 export class AdviceComponent implements OnInit {
+  advice: Advice;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private startRequestService: StartRequestService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id: number = +params['id'];
+      this.startRequestService.makeRequestOfAdviceItem("get", id.toString(), null).subscribe(response => {
+        this.advice = response;
+      })
+    })
   }
 
 }
